d93cb09ed58e23bf09c0992c04638418ef62c95c
Vinicius <>
2020-05-03 19:35:08 -0300

    fixed imports

--- src/components/projects/RetroControllerChangelog.jsx
+++ src/components/projects/RetroControllerChangelog.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import marked from "marked";
 
 import Layout from "../layout";
