8bcb6bafd7a9055cae4441a4446d4db64bf7624d
Vinicius <vncastanheira@gmail.com>
2020-08-23 11:53:50 -0300

    simplifications

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 
-class Layout extends React.Component {
-    render(props){
-        return (
-            <Router>
-                {this.props.children}
-                <div id="background"></div>
-            </Router>
-        )
-    }
-}
+const Layout = ({ children }) => (
+    <Router>
+        {children}
+        <div id="background"></div>
+    </Router>
+)
 
 export default Layout
